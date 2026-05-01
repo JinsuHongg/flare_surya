baaf1caea7d6e339c465877e9870220aa4d6cb9a
Jinsu Hong <jinsuhong.knight@gmail.com>
2026-04-30 22:49:54 -0400

    update shell script for running multi experiments with lrs

---
+++ shell_scripts/nas/lr_exps/submit_lr_sweep_gh.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+# Submit multiple jobs sweeping learning rates for baseline training
+
+TASK_NAME="alexnet_c"
+LEARNING_RATES=(1e-6 5e-6 1e-5 5e-5 1e-4)
+WEIGHT_DECAYS=(0.05)
+
+for lr in "${LEARNING_RATES[@]}"; do
+  for wd in "${WEIGHT_DECAYS[@]}"; do
+    echo "Submitting: $TASK_NAME with lr=$lr wd=$wd"
+    qsub -N "${TASK_NAME}_lr${lr}_wd${wd}" \
+      -v TASK_NAME="${TASK_NAME}",LEARNING_RATE="${lr}",WEIGHT_DECAY="${wd}" \
+      ./shell_scripts/nas/lr_exps/train_lr_sweep.pbs
+  done
+done
